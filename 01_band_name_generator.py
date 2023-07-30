814ac626d1cc1b855763a7c2da8805bec269aa06
JesCad <jesuscadena27@hotmail.com>
2023-07-30 15:58:09 -0600

    creation of  band_name_generator

---
+++ 01_band_name_generator.py
@@ -0,0 +1,14 @@
+#primero se imprime el mensaje de bienvenida
+print("Welcome to the Band Name Generator.")
+
+#se pide al usuario que ingrese el nombre de la ciudad donde crecio
+city = input("What's name of the city you grew up in? ")
+print(city)
+
+#se pide al usuario que ingrese el nombre de su mascota
+pet = input("What's your pet's name? ")
+print()
+
+#se imprime el nombre de la banda
+print("Your band name could be " + city + " " + pet)
+
